6cff3248d53a1081d0a315655307a2fe3b781e89
Vanshika S <vanshika@thepublive.com>
2026-07-10 17:31:53 +0530

    Data entries dump to railway

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python manage.py migrate --fake-initial --noinput
 
 exec python manage.py runserver "0.0.0.0:${PORT:-8000}"
